60c4c1edf97bc5349c4a3f25dd6bf52543865d28
Ihor Barmak <ihor@uplab.io>
2018-10-05 19:11:21 +0300

    Android build fix

--- src/screens/PartnersMap/PartnersMapContainer.js
+++ src/screens/PartnersMap/PartnersMapContainer.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import PartnersMap from './PartnersMap';
 import { PSEUDO_USER_LOCATION } from 'app/constants/globals';
+import markers from 'app/constants/markers';
+// import FastImage from 'react-native-fast-image';
 
 class PartnersMapContainer extends Component {
-  state = {}
+  // state = { ready: false }
+
+  // async componentDidMount(){
+  //   await FastImage.preload(markers.map(({image})=>({uri:image})));
+  //   this.setState({ ready: true });
+  // }
 
   render() {
+    // const { ready } = this.state;
+
+    // if(ready){
     return (
-      <PartnersMap userLocation={PSEUDO_USER_LOCATION} {...this.props}/>
+      <PartnersMap userLocation={PSEUDO_USER_LOCATION} markers={markers} {...this.props}/>
     );
+    // }
+    // return null;
   }
 }
 
