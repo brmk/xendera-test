e99a66950fa937b700f80873c72cc4567108f738
Ihor Barmak <ihor@uplab.io>
2018-10-05 18:02:52 +0300

    removed unused file

--- src/navigators/HeaderTitle.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text } from 'app/components';
-
-const HeaderTitle = (props) => {
-  console.log(props);
-  return (<Text>Hello</Text>)
-}
-export default HeaderTitle;
